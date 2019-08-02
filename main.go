a4e8805cffa78e6e35a62138599951d734c8a226
Roberto Scudeller <roberto.scudeller@rakuten.com>
2019-08-02 14:56:05 +0200

    add conditional to close or add a note to alert in OpsGenie and use Priority from Check.Annotations opsgenie_priority

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	notFound = "NOT FOUND"
+	source   = "sensuGo"
+)
+
 var (
 	authToken string
 	team      string
@@ -55,12 +60,36 @@ func configureRootCommand() *cobra.Command {
 	return cmd
 }
 
+// eventKey func return Entity.Name/Check.Name to use in message and alias
 func eventKey(event *types.Event) string {
 	return fmt.Sprintf("%s/%s", event.Entity.Name, event.Check.Name)
 }
 
+// eventPriority func read priority in the event and return alerts.PX
+// check.Annotations override Entity.Annotations
 func eventPriority(event *types.Event) alertsv2.Priority {
-	if event.Entity.Annotations != nil {
+	if event.Check.Annotations != nil {
+		switch event.Check.Annotations["opsgenie_priority"] {
+		case "P5":
+			return alerts.P5
+
+		case "P4":
+			return alerts.P4
+
+		case "P3":
+			return alerts.P3
+
+		case "P2":
+			return alerts.P2
+
+		case "P1":
+			return alerts.P1
+
+		default:
+			return alerts.P3
+
+		}
+	} else if event.Entity.Annotations != nil {
 		switch event.Entity.Annotations["opsgenie_priority"] {
 		case "P5":
 			return alerts.P5
@@ -87,6 +116,7 @@ func eventPriority(event *types.Event) alertsv2.Priority {
 
 }
 
+// run func do everything
 func run(cmd *cobra.Command, args []string) error {
 	if len(args) != 0 {
 		_ = cmd.Help()
@@ -128,10 +158,24 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("event does not contain check")
 	}
 
-	return manageIncident(event)
+	// check if event has a alert
+	hasAlert, _ := getAlert(event)
+	// fmt.Printf("Has Alert: %s \n", hasAlert)
+	if hasAlert == notFound && event.Check.Status != 0 {
+		return createIncident(event)
+	}
+
+	if hasAlert != notFound && event.Check.Status == 0 {
+		return closeAlert(event, hasAlert)
+	}
+	if event.Check.Status != 0 {
+		return addNote(event, hasAlert)
+	}
+	return nil
 }
 
-func manageIncident(event *types.Event) error {
+// createIncident func create an alert in OpsGenie
+func createIncident(event *types.Event) error {
 
 	cli := new(ogcli.OpsGenieClient)
 	cli.SetAPIKey(authToken)
@@ -144,11 +188,11 @@ func manageIncident(event *types.Event) error {
 
 	request := alerts.CreateAlertRequest{
 		Message:     eventKey(event),
-		Alias:       event.Check.Name,
+		Alias:       eventKey(event),
 		Description: event.Check.Output,
 		Teams:       teams,
 		Entity:      event.Entity.Name,
-		Source:      "sensu",
+		Source:      source,
 		Priority:    eventPriority(event),
 	}
 
@@ -158,5 +202,79 @@ func manageIncident(event *types.Event) error {
 	} else {
 		fmt.Println("Create request ID: " + response.RequestID)
 	}
+	return nil
+}
+
+// getAlert func get a alert using an alias.
+func getAlert(event *types.Event) (string, error) {
+
+	cli := new(ogcli.OpsGenieClient)
+	cli.SetAPIKey(authToken)
+
+	alertCli, _ := cli.AlertV2()
+
+	response, err := alertCli.Get(alerts.GetAlertRequest{
+		Identifier: &alerts.Identifier{
+			Alias: eventKey(event),
+		},
+	})
+
+	if err != nil {
+		return notFound, nil
+	}
+	alert := response.Alert
+	fmt.Printf("ID: %s, Message: %s ,Count: %d \n", alert.ID, alert.Message, alert.Count)
+	return alert.ID, nil
+}
+
+// closeAlert func close an alert if status == 0
+func closeAlert(event *types.Event, alertid string) error {
+	cli := new(ogcli.OpsGenieClient)
+	cli.SetAPIKey(authToken)
+
+	alertCli, _ := cli.AlertV2()
+
+	identifier := alerts.Identifier{
+		ID: alertid,
+	}
+	closeRequest := alerts.CloseRequest{
+		Identifier: &identifier,
+		Source:     source,
+		Note:       "Closed Automatically",
+	}
+
+	response, err := alertCli.Close(closeRequest)
+
+	if err != nil {
+		fmt.Printf("[ERROR] Not Closed: %s", err)
+	}
+	fmt.Printf("RequestID %s to Close %s", alertid, response.RequestID)
+
+	return nil
+}
+
+// addNode func add a note inside an alert if status != 0
+func addNote(event *types.Event, alertid string) error {
+	cli := new(ogcli.OpsGenieClient)
+	cli.SetAPIKey(authToken)
+
+	alertCli, _ := cli.AlertV2()
+
+	note := fmt.Sprintf("Last Check Status: %s, Output: %s", event.Check.State, event.Check.Output)
+
+	request := alerts.AddNoteRequest{
+		Identifier: &alerts.Identifier{
+			ID: alertid,
+		},
+		Source: source,
+		Note:   note,
+	}
+
+	response, err := alertCli.AddNote(request)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("RequestID: " + response.RequestID)
 	return nil
 }
